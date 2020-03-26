7b039c0f4ca6261c2084a70943093dea34f53850
romain <romain.soumard@etu.univ-amu.fr>
2020-03-26 23:48:27 +0100

    Formatage du fichier

--- src/Serveur/SalonCentral.java
+++ src/Serveur/SalonCentral.java
@@ -11,9 +11,10 @@ public class SalonCentral extends AbstractSelectorServer {
         super(port);
     }
 
-
     /**
-     * Treats acceptable keys
+     * Treats acceptable keys. Accepts incoming connections from a client.
+     * Since the servers's channel is the only one registered with OP_ACCEPT,
+     * It is the only one to ever use this method.
      *
      * @param key the key from which we get the channel to treat
      * @throws IOException
@@ -28,9 +29,9 @@ public class SalonCentral extends AbstractSelectorServer {
         }
     }
 
-
     /**
-     * Treats readable keys
+     * Treats readable keys. Reads from the buffer and checks the content
+     * of the message to see if there is any protocol violation.
      *
      * @param key the key from which we get the channel to treat
      * @throws IOException
@@ -56,18 +57,27 @@ public class SalonCentral extends AbstractSelectorServer {
         }
     }
 
-
     private boolean isLogin(SocketChannel client, String[] loginParts) {
         return loginParts[0].equals("LOGIN") && !clientPseudos.containsKey(client);
     }
-
 
     private void registerLogin(SocketChannel client, String[] messageParts) {
         clientPseudos.put(client, messageParts[1]);
         System.out.println(String.join(" ", messageParts));
     }
 
-
+    /**
+     * Treats the incoming message by applying security checks on it.
+     * If a message has the good format, then it is printed, otherwise
+     * we check whether the client was registered. If he was not, then, then we
+     * send a login error back to the client.
+     * If the client was registered, then it means it is simpply a message error.
+     *
+     * @param client       The client that we listen
+     * @param key          The key corresponding to the client
+     * @param messageParts A string array containing all parts of the message
+     * @throws IOException
+     */
     private void treatMessage(SocketChannel client, SelectionKey key, String[] messageParts) throws IOException {
         if (isMessage(messageParts))
             System.out.println(clientPseudos.get(client) + ": " + String.join(" ", messageParts));
@@ -79,18 +89,16 @@ public class SalonCentral extends AbstractSelectorServer {
             sendErrorMessage("ERROR chatamu\n");
     }
 
-
     private boolean isMessage(String[] messageParts) {
         return messageParts[0].equals("MESSAGE") && messageParts[messageParts.length - 1].equals("envoye");
     }
-
 
     private boolean isRegistered(SocketChannel client) {
         return clientPseudos.containsKey(client);
     }
 
-
     @Override
     protected void treatWritable(SelectionKey key) {
     }
+
 }
