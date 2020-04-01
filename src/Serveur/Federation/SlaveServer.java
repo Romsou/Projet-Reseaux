45ca3d61c8bd57b5db3f474ec00f863bae9cc30f
romain <romain.soumard@etu.univ-amu.fr>
2020-04-01 19:46:46 +0200

    SlaveServer premier jet

--- src/Serveur/Federation/SlaveServer.java
+++ src/Serveur/Federation/SlaveServer.java
@@ -1,42 +1,70 @@
 package Serveur.Federation;
 
-import Serveur.AbstractServers.AbstractSelectorServer;
+import Protocol.ProtocolHandler;
+import Serveur.ChatAmuCentral.ChatamuCentral;
 
 import java.io.IOException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.util.HashMap;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class SlaveServer extends AbstractSelectorServer {
-    HashMap<SocketChannel, ConcurrentLinkedDeque> clientQueue;
-    private SocketChannel masterServer;
+public class SlaveServer extends ChatamuCentral {
+    private HashMap<SocketChannel, SocketChannel> masterServers;
 
 
     public SlaveServer(int port) {
         super(port);
-        this.clientQueue = new HashMap<>();
-    }
-
-    @Override
-    protected void writeMessageToClients(String message) {
-
-    }
-
-    @Override
-    protected void treatReadable(SelectionKey key) throws IOException {
-
     }
 
     @Override
     protected void treatAcceptable(SelectionKey key) throws IOException {
-
+        if (key.isAcceptable())
+            acceptIncomingConnections(key);
     }
 
     @Override
-    protected void treatWritable(SelectionKey key) throws IOException {
+    protected void treatReadable(SelectionKey key) {
+        if (key.isReadable()) {
+            if (!client.equals(key.channel()))
+                client = (SocketChannel) key.channel();
+
+            if (buffer.read(client) >= 0) {
+                String message = buffer.convertBufferToString();
+                String[] messageParts = message.split(" ");
+
+                if (ProtocolHandler.isServerConnection(message)) {
+                    masterServers.put(client, client);
+                    return;
+                }
+
+                if (isLogin(client, messageParts)) {
+                    registerLogin(client, messageParts);
+                    clientQueue.put(client, new ConcurrentLinkedQueue<>());
+                    return;
+                }
+
+                if (ProtocolHandler.isMessage(messageParts) && !masterServers.containsKey(client)) {
+                    for (SocketChannel masterServer : masterServers.keySet())
+                        sendMessage(masterServer, message);
+                }
+
+
+                if (ProtocolHandler.isMessage(messageParts) && masterServers.containsKey(client)) {
+                    broadcast(message);
+                    return;
+                }
+
+                if (!isRegistered(client)) {
+                    sendMessage(client, ProtocolHandler.ERROR_LOGIN.concat("\n"));
+                    key.cancel();
+                    return;
+                }
+
+                sendMessage(client, ProtocolHandler.ERROR_MESSAGE.concat("\n"));
+
+            }
+        }
 
     }
-
-
 }
