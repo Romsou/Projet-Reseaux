e8b35c247497abda0074e383922bd5babc63b56c
romain <romain.soumard@etu.univ-amu.fr>
2020-04-03 05:45:58 +0200

    Effacement de classes inutiles

--- src/Serveur/Federation/MasterServer.java
+++
@@ -1,9 +0,0 @@
-package Serveur.Federation;
-
-import java.io.IOException;
-
-public class MasterServer extends AbstractMasterServer {
-    public MasterServer() throws IOException {
-        super(12347);
-    }
-}
